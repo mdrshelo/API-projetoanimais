850194657c446379bb9001c841640bb9e20b5e24
HELOISA CRISTINA FONSECA MEDEIROS <a95696359@rn.senac.local>
2026-09-18 11:24:57 -0300

    primeira rota da API

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+
+export default app;
